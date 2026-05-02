19d681dacec363b99c157bbe9788954c695bb70e
Sofia Kohut <ssofia.kohut@gmail.com>
2026-05-02 23:02:05 +0300

    fix

--- tests/product.spec.ts
+++ tests/product.spec.ts
@@ -1,17 +1,17 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../pages/home.pages';
-
+import { ProductPage } from '../pages/product.pages';
 
 test('Verify user can view product details', async ({ page }) => {
-    const homePage = new HomePage(page);
-   
-    await homePage.navigate();
-    await page.getByText('Combination Pliers').click();
-    
-    
-    await expect(page).toHaveURL(/product/);
-    await expect(page.locator('[data-test="product-name"]')).toHaveText('Combination Pliers');
-    await expect(page.locator('[data-test="unit-price"]')).toHaveText('14.15');
-    await expect(page.locator('[data-test="add-to-cart"]')).toBeVisible();
-    await expect(page.locator('[data-test="add-to-favorites"]')).toBeVisible();
-})
+  const homePage = new HomePage(page);
+  const productPage = new ProductPage(page);
+
+  await homePage.navigate();
+  await page.getByText('Combination Pliers').click();
+
+  await expect(page).toHaveURL(/product/);
+  await expect(productPage.productName).toHaveText('Combination Pliers');
+  await expect(productPage.unitPrice).toHaveText('14.15');
+  await expect(productPage.addToCart).toBeVisible();
+  await expect(productPage.addToFavorites).toBeVisible();
+});
