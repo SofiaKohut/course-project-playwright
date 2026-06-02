47a771742fc7ba9739b672fd72612269e2887c1d
Sofia Kohut <ssofia.kohut@gmail.com>
2026-06-02 22:23:26 +0300

    another CI fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,6 +20,8 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
+    actionTimeout: 60000,
+    navigationTimeout: 60000,
   },
   projects: [
     {
